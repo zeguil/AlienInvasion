bdec6f4507050e22708e8b11db22c9891335be3b
zeguil <zeguilhermelins@hotmail.com>
2021-11-07 16:02:38 -0300

    second

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 import sys
+from settings import Settings
+from ship import Ship
 import pygame
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    settings = Settings()
+    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     pygame.display.set_caption("Invasão Alien")
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT():
                 sys.exit()
-        
+        screen.fill(settings.bg_color)
         pygame.display.flip()
 
 run_game()
